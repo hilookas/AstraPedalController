289255578c93fc0c6ec8b5420844d1ae3b9d8dbe
lookas <i@18kas.com>
2024-06-15 20:11:28 +0800

    Add ADC Support

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,25 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // initialize serial communication at 115200 bits per second:
+  Serial.begin(115200);
+  
+  //set the resolution to 12 bits (0-4096)
+  analogReadResolution(12);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // ADC 2 is used by wifi
+  // see: https://blog.mjyai.com/2022/10/28/esp32-arduino-ads1115-adc-benchmark/
+  int analogVolts = analogReadMilliVolts(36);
+  int analogVolts2 = analogReadMilliVolts(39);
+  int analogVolts3 = analogReadMilliVolts(34);
+  int analogVolts4 = analogReadMilliVolts(35);
+  int analogVolts5 = analogReadMilliVolts(32);
+  int analogVolts6 = analogReadMilliVolts(33);
+  
+  // print out the values you read:
+  Serial.printf("ADC millivolts value = %d %d %d %d %d %d\n", analogVolts, analogVolts2, analogVolts3, analogVolts4, analogVolts5, analogVolts6);
+  
+  delay(100);  // delay in between reads for clear read from serial
 }
